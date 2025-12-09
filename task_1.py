150ac7f778b9b4267c4b241f8988c7636525ad0a
Khrystyna Bash <khrystynabash09@gmail.com>
2025-12-09 11:20:06 +0200

    доповнення коду. кінець першої задачі.

--- task_1.py
+++ task_1.py
@@ -2,6 +2,7 @@ def convert_to_uah(amount, rate=41.5 ):
     return(amount*rate)
 standart = convert_to_uah(100, 41.5)
 other = convert_to_uah(100, 38.0)
-
+print("курс 100$ за стандартним курсом:", standart, "грн")
+print("курс 100$ за іншим курсом:", other, "грн")
 
 
