fc4a4dbdd3476bba0ac440b741f6ac5d3897756c
Khrystyna Bash <khrystynabash09@gmail.com>
2025-12-09 11:26:05 +0200

    2 завдання. кпочаток коду.

---
+++ task_2.py
@@ -0,0 +1,11 @@
+def get_grade(score):
+    if grade > 89:
+        print("A")
+    elif grade > 79:
+        print("B")
+    elif grade > 69:
+        print("C")
+    elif grade > 59:
+        print("D")
+    elif grade < 60:
+        print("F")
