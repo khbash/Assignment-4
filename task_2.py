bffea8ce6cf1664470d5b1267d1481cb01926551
Khrystyna Bash <khrystynabash09@gmail.com>
2025-12-09 14:06:22 +0200

    2 завдання. виконане.

--- task_2.py
+++ task_2.py
@@ -1,11 +1,15 @@
 def get_grade(score):
-    if grade > 89:
-        print("A")
-    elif grade > 79:
-        print("B")
-    elif grade > 69:
-        print("C")
-    elif grade > 59:
-        print("D")
-    elif grade < 60:
-        print("F")
+    if score > 89:
+        return("A")
+    elif score > 79:
+        return("B")
+    elif score > 69:
+        return("C")
+    elif score > 59:
+        return("D")
+    elif score < 60:
+        return("F")
+
+scores =[95, 45, 78, 82, 60]
+grades = [get_grade(i) for i in scores]
+print(grades)
