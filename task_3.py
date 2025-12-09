c62605370ecc5706551aa0cc3492439661eaf1e7
Khrystyna Bash <khrystynabash09@gmail.com>
2025-12-09 14:23:12 +0200

    3 завдання. код

---
+++ task_3.py
@@ -0,0 +1,12 @@
+def process_princes(prices, discount_percent):
+    new = []
+
+    for price in prices:
+        if price > 0:
+            discount = price * (1 - discount_percent)//100
+            new.append(round(discount))
+    return(new)
+
+old = [1000, -50, 300, 0, 500]
+result=process_princes(old, 20)
+print(result)
